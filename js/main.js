35dd40c1d1e2970dd0146cb1ddd995e3443e4373
sleach2 <sleach2@gmu.edu>
2015-04-11 21:24:58 -0400

    cd

--- js/main.js
+++ js/main.js
@@ -31,6 +31,16 @@ window.onload = function() {
     }
     
     function update() {
-       
+        game.physics.arcade.collide(player, platforms);
+        player.body.velocity.x=0;
+        if(cursors.left.isDown){
+            player.body.velocity.x = -250;
+        }else if(cursors.right.isDown){
+            player.body.velocity.x = 250;
+        }else if(cursors.up.isDown){
+            player.body.velocity.y = -250;
+        }else if(cursors.down.isDown){
+            player.body.velocity.y = 250;
+        }
     }
 };
